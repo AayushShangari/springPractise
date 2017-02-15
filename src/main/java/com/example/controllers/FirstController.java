a043d82c375f1dd671683b1d7d0b95e3f88691ca
Aayush Shangari <ShangariAayush@JohnDeere.com>
2017-02-15 21:41:02 +0530

    second commit

---
+++ src/main/java/com/example/controllers/FirstController.java
@@ -0,0 +1,11 @@
+package com.example.controllers;
+
+import java.lang.annotation.Annotation;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+	
+	
+
+}
---
+++ src/main/java/com/example/services/FirstService.java
@@ -0,0 +1,11 @@
+package com.example.services;
+
+import java.lang.annotation.Annotation;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirstService {
+	
+	
+
+}
